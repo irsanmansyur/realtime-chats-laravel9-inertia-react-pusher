ea60aea6e3d547c7ccfa2b1bc950a21647112f32
Irsan Mansyur <irsan00mansyur@gmail.com>
2022-06-03 20:27:09 +0800

    update route

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,https://chats.jagohasil.com,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,http://chats.jagohasil.com,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
